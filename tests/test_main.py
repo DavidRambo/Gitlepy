6e8c82befe5171cb889d42ddc95526ee60f2f8c7
David Rambo <davidrambo@mailfence.com>
2023-02-10 15:36:55 -0800

    Remove init command from test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-# tests/main_tests.py
+# tests/test_main.py
 """Tests the main.py module."""
 from click.testing import CliRunner
 import pytest
@@ -15,9 +15,9 @@ def runner():
     ("cmd", "expected"),
     (
         pytest.param([""], "Incorrect operands.\n", id="empty_arg"),
-        pytest.param(
-            ["init"], "Initializing gitlepy repository.\n", id="init_new_print"
-        ),
+        # pytest.param(
+        #     ["init"], "Initializing gitlepy repository.\n", id="init_new_print"
+        # ),
         # pytest.param(
         #     ["init"], "Gitlepy repository already exists.\n", id="init_already_exists"
         # ),
@@ -37,12 +37,3 @@ def test_main_print_statements(runner, cmd, expected):
 #     out, err = capsys.readouterr()
 #     assert out == "Incorrect operands."
 #     assert err == ""
-
-
-def test_init_new_repo(runner):
-    """Tries to initialize a new Gitlepy repository.
-    Uses click.testing's CliRunner.isolated_filesystem() to create a temporary
-    directory in which gitlepy sets up its repository.
-    https://click.palletsprojects.com/en/8.1.x/testing/#file-system-isolation
-    """
-    pass
