4cd2887e7bb69d031ce81efa28f2fadb81507b43
David Rambo <davrambo@gmail.com>
2023-02-12 19:21:28 -0800

    Monkeypatch testing dir to be CWD

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import shutil
+import pytest
+
+from gitlepy import repository as repo
+
+
+@pytest.fixture(autouse=True)
+def change_dir(request, monkeypatch):
+    """Changes the CWD to the test directory."""
+    monkeypatch.chdir(request.fspath.dirname)
+
+
+@pytest.fixture(autouse=True)
+def setup_teardown():
+    """Cleans up the Gitlepy repository for subsequent tests."""
+    gitlepydir = repo.GITLEPY_DIR
+    if gitlepydir.exists():
+        shutil.rmtree(gitlepydir)
+    yield
+    if gitlepydir.exists():
+        shutil.rmtree(gitlepydir)
--- tests/test_init.py
+++ tests/test_init.py
@@ -15,44 +15,36 @@ def runner():
     return CliRunner()
 
 
-@pytest.fixture(autouse=True)
-def setup_teardown():
-    """Cleans up the Gitlepy repository for subsequent tests."""
-    gitlpydir = repo.GITLEPY_DIR
-    if gitlpydir.exists():
-        shutil.rmtree(gitlpydir)
-    yield
-    if gitlpydir.exists():
-        shutil.rmtree(gitlpydir)
-
-
-def test_main_init_new_repo(runner, tmp_path):
+def test_main_init_new_repo(runner):
     """Creates a new repository successfully.
     Uses click.testing's CliRunner.isolated_filesystem() to create a temporary
     directory in which gitlepy sets up its repository.
     https://click.palletsprojects.com/en/8.1.x/testing/#file-system-isolation
     """
-    with runner.isolated_filesystem(tmp_path):
-        result = runner.invoke(main, ["init"])
-        assert repo.GITLEPY_DIR.exists()
-        assert repo.BLOBS_DIR.exists()
-        assert repo.COMMITS_DIR.exists()
-        assert repo.INDEX.exists()
+    # with runner.isolated_filesystem(tmp_path):
+    #     print(f"\n>>>>\n{tmp_path=}\n<<<<<\n")
+    print(f"\n<<<<\n{Path.cwd()=}\n>>>>>\n")
+    result = runner.invoke(main, ["init"])
+    assert repo.GITLEPY_DIR.exists()
+    assert repo.BLOBS_DIR.exists()
+    assert repo.COMMITS_DIR.exists()
+    assert repo.INDEX.exists()
 
-        main_branch = Path(repo.BRANCHES / "main")
-        assert main_branch.exists()
+    main_branch = Path(repo.BRANCHES / "main")
+    assert main_branch.exists()
 
-        assert repo.HEAD.exists()
-        assert repo.HEAD.read_text() == "main"
+    assert repo.HEAD.exists()
+    assert repo.HEAD.read_text() == "main"
 
-        assert result.exit_code == 0
-        assert result.output == "Initializing gitlepy repository.\n"
+    assert result.exit_code == 0
+    assert result.output == "Initializing gitlepy repository.\n"
 
 
-def test_main_init_already_exists(runner, tmp_path):
+def test_main_init_already_exists(runner):
     """Tries to create a new repository where one already exists."""
-    with runner.isolated_filesystem():
-        repo.GITLEPY_DIR.mkdir()
-        result = runner.invoke(main, ["init"])
-        assert result.exit_code == 0
-        assert result.output == "Gitlepy repository already exists.\n"
+    # with runner.isolated_filesystem():
+    # print(tmp_path)
+    repo.GITLEPY_DIR.mkdir()
+    result = runner.invoke(main, ["init"])
+    assert result.exit_code == 0
+    assert result.output == "Gitlepy repository already exists.\n"
