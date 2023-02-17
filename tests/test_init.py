22c1f07b548cb4cbe171cc7f60b329de54ce554f
David Rambo <davrambo@gmail.com>
2023-02-17 10:35:11 -0800

    Add tests for init's index and initial commit

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,17 @@
 # tests/test_init.py
-"""Tests the init command."""
+"""Tests the init command.
+Tests for the creation of the repository directory and subdirectories,
+the staging area file, the initial commit object, that the HEAD file exists
+and points to the main branch, and that the main branch file exists and
+points to the initial commit.
+"""
 from pathlib import Path
+import pickle
 
 from click.testing import CliRunner
 import pytest
 
+from gitlepy.index import Index
 from gitlepy.__main__ import main
 import gitlepy.repository as repo
 
@@ -15,11 +22,7 @@ def runner():
 
 
 def test_main_init_new_repo(runner):
-    """Creates a new repository successfully.
-    Uses click.testing's CliRunner.isolated_filesystem() to create a temporary
-    directory in which gitlepy sets up its repository.
-    https://click.palletsprojects.com/en/8.1.x/testing/#file-system-isolation
-    """
+    """Creates a new repository successfully."""
     # with runner.isolated_filesystem(tmp_path):
     #     print(f"\n>>>>\n{tmp_path=}\n<<<<<\n")
     # print(f"\n<<<<\n{Path.cwd()=}\n>>>>>\n")
@@ -28,6 +31,10 @@ def test_main_init_new_repo(runner):
     assert repo.BLOBS_DIR.exists()
     assert repo.COMMITS_DIR.exists()
     assert repo.INDEX.exists()
+
+    with open(repo.INDEX, "rb") as file:
+        test_index: Index = pickle.load(file)
+        assert repr(test_index) == "Index"
 
     main_branch = Path(repo.BRANCHES / "main")
     assert main_branch.exists()
@@ -47,3 +54,26 @@ def test_main_init_already_exists(runner):
     result = runner.invoke(main, ["init"])
     assert result.exit_code == 0
     assert result.output == "Gitlepy repository already exists.\n"
+
+
+def test_index_exists(runner):
+    """Ensures that the Index exists and is an instance of the Index class."""
+    runner.invoke(main, ["init"])
+    assert repo.INDEX.exists()
+    with open(repo.INDEX, "rb") as file:
+        test_index: Index = pickle.load(file)
+        assert repr(test_index) == "Index"
+
+
+def test_initial_commit(runner):
+    """Ensures that the initial commit was created correctly."""
+    runner.invoke(main, ["init"])
+    assert repo.COMMITS_DIR.exists()
+    # Get name of commit object file. There should be only one.
+    all_commits = list(repo.COMMITS_DIR.iterdir())
+    commit_file = all_commits[0]
+    # Open it and unpickle it.
+    with open(commit_file, "rb") as file:
+        test_commit: Commit = pickle.load(file)
+        assert repr(test_commit) == "Commit"
+        assert test_commit.message == "Initial commit."
