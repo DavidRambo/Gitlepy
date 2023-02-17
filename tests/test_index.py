17b86d93a8656f58f669176ecdef9114dc6fb5b6
David Rambo <davrambo@gmail.com>
2023-02-17 12:09:55 -0800

    Write two simple add tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -16,10 +16,37 @@ def runner():
     return CliRunner()
 
 
-def test_index_saved(runner):
-    """Creates a new Gitlepy repository and checks for the index."""
+@pytest.fixture
+def test_index(runner):
     runner.invoke(main, ["init"])
     assert repo.INDEX.exists()
     with open(repo.INDEX, "rb") as file:
         test_index: Index = pickle.load(file)
         assert repr(test_index) == "Index"
+    return test_index
+
+
+def test_add(runner, test_index):
+    """Adds a file to the staging area.
+
+    Args:
+        runner (CliRunner): This is the click.testing object used to invoke
+            commands.
+        test_index (Index): The Gitlepy repository's Index object.
+    """
+    # Create a new file and write some text to it.
+    file_a = Path(Path.cwd() / "a.txt")
+    file_a.touch()
+    file_a.write_text("hello")
+    # gitlepy add a.txt
+    runner.invoke(main, ["add", "a.txt"])
+    adds = test_index.additions
+    assert len(adds) == 1
+    assert "a.txt" in adds.keys()
+
+
+def test_add_no_file(runner, test_index):
+    """Tries to add a non-existent file to the staging area."""
+    result = runner.invoke(main, ["add", "nofile"])
+    assert result.exit_code == 1
+    assert result.output == "nofile does not exist.\n"
