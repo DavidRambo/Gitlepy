a5469abe97bbffef95f6c430cdc34f121c5b2123
David Rambo <davrambo@gmail.com>
2023-02-18 17:44:42 -0800

    Set repository Path variables in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import shutil
+import tempfile
+
 import pytest
 
 from gitlepy import repository as repo
@@ -8,15 +11,26 @@ from gitlepy import repository as repo
 def change_dir(request, monkeypatch, tmp_path):
     """Changes the CWD to the test directory."""
     # monkeypatch.chdir(request.fspath.dirname)
-    monkeypatch.chdir(tmp_path)
+    # monkeypatch.chdir(tmp_path)
+    repo.WORK_DIR = Path(tmp_path)
+    repo.GITLEPY_DIR = Path(tmp_path / ".gitlepy")
+    repo.BLOBS_DIR = Path(repo.GITLEPY_DIR / "blobs")
+    repo.COMMITS_DIR = Path(repo.GITLEPY_DIR / "commits")
+    repo.BRANCHES = Path(repo.GITLEPY_DIR / "refs")
+    repo.INDEX = Path(repo.GITLEPY_DIR / "index")
+    repo.HEAD = Path(repo.GITLEPY_DIR / "HEAD")
 
 
-@pytest.fixture(autouse=True)
-def setup_teardown():
-    """Cleans up the Gitlepy repository for subsequent tests."""
-    gitlepydir = repo.GITLEPY_DIR
-    if gitlepydir.exists():
-        shutil.rmtree(gitlepydir)
-    yield
-    if gitlepydir.exists():
-        shutil.rmtree(gitlepydir)
+# @pytest.fixture(autouse=True)
+# def setup_teardown():
+#     """Cleans up the Gitlepy repository for subsequent tests."""
+#     gitlepydir = repo.GITLEPY_DIR
+#     print(f"\n>>>within setup_teardown, cwd={Path.cwd()}")
+#     print(f"\n{repo.WORK_DIR=}")
+#     print(f"\n{repo.GITLEPY_DIR=}")
+#     print(f"\n{repo.BLOBS_DIR=}")
+#     if gitlepydir.exists():
+#         shutil.rmtree(gitlepydir)
+#     yield
+#     if gitlepydir.exists():
+#         shutil.rmtree(gitlepydir)
