036d0a5c54ef81fbfc6bc0e3745e6b3d8590fb6a
David Rambo <davidrambo@mailfence.com>
2023-02-21 16:57:36 -0800

    Return to monkeypatching tmpdirs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,23 @@
 from pathlib import Path
-import shutil
-import tempfile
+
+# import shutil
+# import tempfile
 
 import pytest
-
-from gitlepy import repository as repo
 
 
 @pytest.fixture(autouse=True)
 def change_dir(request, monkeypatch, tmp_path):
     """Changes the CWD to the test directory."""
-    # monkeypatch.chdir(request.fspath.dirname)
-    # monkeypatch.chdir(tmp_path)
-    repo.WORK_DIR = Path(tmp_path)
-    repo.GITLEPY_DIR = Path(tmp_path / ".gitlepy")
-    repo.BLOBS_DIR = Path(repo.GITLEPY_DIR / "blobs")
-    repo.COMMITS_DIR = Path(repo.GITLEPY_DIR / "commits")
-    repo.BRANCHES = Path(repo.GITLEPY_DIR / "refs")
-    repo.INDEX = Path(repo.GITLEPY_DIR / "index")
-    repo.HEAD = Path(repo.GITLEPY_DIR / "HEAD")
+    monkeypatch.chdir(request.fspath.dirname)
+    monkeypatch.chdir(tmp_path)
+    # repo.WORK_DIR = Path(tmp_path)
+    # repo.GITLEPY_DIR = Path(tmp_path / ".gitlepy")
+    # repo.BLOBS_DIR = Path(repo.GITLEPY_DIR / "blobs")
+    # repo.COMMITS_DIR = Path(repo.GITLEPY_DIR / "commits")
+    # repo.BRANCHES = Path(repo.GITLEPY_DIR / "refs")
+    # repo.INDEX = Path(repo.GITLEPY_DIR / "index")
+    # repo.HEAD = Path(repo.GITLEPY_DIR / "HEAD")
 
 
 # @pytest.fixture(autouse=True)
