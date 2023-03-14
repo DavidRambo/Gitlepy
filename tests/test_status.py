d066fbbac905d6105faa101ea83b97ada4aaa887
David Rambo <davidrambo@mailfence.com>
2023-03-14 14:35:53 -0700

    Add tests for unstaged modifications Repo method

--- tests/test_status.py
+++ tests/test_status.py
@@ -9,7 +9,7 @@ from gitlepy.__main__ import main
 from gitlepy.repository import Repo
 
 
-def test_working_files(runner, setup_repo):
+def test_working_files(setup_repo):
     """Tests Repo.working_files property method."""
     repo = Repo(Path.cwd())
     file_a = Path(setup_repo["work_path"] / "a.txt")
@@ -18,7 +18,7 @@ def test_working_files(runner, setup_repo):
     assert ["a.txt"] == working_result
 
 
-def test_untracked_files(runner, setup_repo):
+def test_untracked_files(setup_repo):
     """Tests Repo.untracked_files property method."""
     repo = Repo(Path.cwd())
     file_a = Path(setup_repo["work_path"] / "a.txt")
@@ -28,3 +28,96 @@ def test_untracked_files(runner, setup_repo):
     assert type(untracked_result) == list
     assert untracked_result is not None
     assert expected == untracked_result
+
+
+def test_unstaged_modifications_modified(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method by modifying a
+    tracked file without staging for addition.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "Add a.txt"])
+    file_a.write_text("hello")
+    result = repo.unstaged_modifications
+    expected = ["a.txt (modified)"]
+    assert expected == result
+
+
+def test_unstaged_modifications_deleted(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method by deleting
+    a tracked file without staging for removal.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "Add a.txt"])
+    file_a.unlink()
+    result = repo.unstaged_modifications
+    expected = ["a.txt (deleted)"]
+    assert expected == result
+
+
+def test_unstaged_modifications_untracked_staged_modified(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method by modifying
+    an untracked file after it was staged for addition.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    file_a.write_text("hello")
+    result = repo.unstaged_modifications
+    expected = ["a.txt (modified)"]
+    assert expected == result
+
+
+def test_unstaged_modifications_tracked_staged_modified(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method by modifying
+    a file tracked by the current commit and already staged for addition.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "Add a.txt"])
+    file_a.write_text("hello")
+    runner.invoke(main, ["add", "a.txt"])
+    file_a.write_text("hello, world")
+    result = repo.unstaged_modifications
+    expected = ["a.txt (modified)"]
+    assert expected == result
+
+
+def test_unstaged_modifications_none(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method when no modifications
+    are unstaged.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "Add a.txt"])
+    file_a.write_text("hello")
+    runner.invoke(main, ["add", "a.txt"])
+    result = repo.unstaged_modifications
+    expected = []
+    assert expected == result
+
+
+def test_unstaged_modifications_removal_readded(runner, setup_repo):
+    """Tests Repo.unstaged_modifications property method by staging a file
+    for removal and then adding it back to the working directory.
+    """
+    repo = Repo(Path.cwd())
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "Add a.txt"])
+    runner.invoke(main, ["rm", "a.txt"])
+    file_a.touch()
+    result = repo.unstaged_modifications
+    expected = ["a.txt (modified)"]
+    assert expected == result
