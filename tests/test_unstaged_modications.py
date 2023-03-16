5fafee261533c1b6bee2d686cd85c8daf147a5f2
David Rambo <davidrambo@mailfence.com>
2023-03-16 11:35:13 -0700

    Create basic tests for unstaged_modifications property.

---
+++ tests/test_unstaged_modications.py
@@ -0,0 +1,34 @@
+"""tests/test_unstaged_modifications.py
+
+Tests the Repo class's unstaged_modifications property method.
+"""
+from pathlib import Path
+
+from gitlepy.__main__ import main
+from gitlepy.repository import Repo
+
+
+def test_unstaged_modifications(runner, setup_repo):
+    r = Repo(Path(setup_repo["work_path"]))
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "create a.txt"])
+    file_a.write_text("Hello")
+    result = r.unstaged_modifications
+    expected = ["a.txt (modified)"]
+    assert expected == result
+
+
+def test_staged_modifications(runner, setup_repo):
+    """Stages for addition and ensures unstaged is empty."""
+    r = Repo(Path(setup_repo["work_path"]))
+    file_a = Path(setup_repo["work_path"] / "a.txt")
+    file_a.touch()
+    runner.invoke(main, ["add", "a.txt"])
+    runner.invoke(main, ["commit", "create a.txt"])
+    file_a.write_text("Hello")
+    runner.invoke(main, ["add", "a.txt"])
+    result = r.unstaged_modifications
+    expected = []
+    assert expected == result
